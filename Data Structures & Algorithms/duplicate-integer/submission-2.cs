1e7f3f1a3a67137d4bba222e905f38cc6d0ce4ad
Shivam Parab <shivamparab21500@gmail.com>
2026-05-08 10:12:31 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        List<int> exists = new List<int>();
+        foreach (int a in nums) {
+            if (exists.Contains(a)) {
+                return true;
+            }
+            exists.Add(a);
+        }
+        return false;
+
+        // return new HashSet<int>(nums).Count < nums.Length;
+    }
+}
